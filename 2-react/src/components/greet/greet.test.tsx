890ee43d09a158b403099c9e0274e78b6c61ab06
ng1n3 <code.with.muyiwa@gmail.com>
2024-11-25 23:37:57 +0100

    test driven development

--- 2-react/src/components/greet/greet.test.tsx
+++ 2-react/src/components/greet/greet.test.tsx
@@ -6,3 +6,13 @@ test("Greet renders correctly", () =>  {
   const text = screen.getByText(/Hello/i)
   expect(text).toBeInTheDocument()
 })
+
+test("greet renders with a name", () => {
+  render(<Greet name="Emmanuel"/>)
+  const textElement  = screen.getByText("Hello Emmanuel")
+  expect(textElement).toBeInTheDocument()
+})
+
+/*
+[LECTURE] -> Testing Driven development is writing test before software code
+*/
